8f0a435aa42b9f1a1b47ff1922bb17ee79d798b9
yuanxinghua <yuanxinghua@reworldgame.com>
2021-07-27 19:23:25 +0800

    优化用例

--- cases/login/test_login.py
+++ cases/login/test_login.py
@@ -12,7 +12,9 @@ class TestLogin:
     # 手机号登录成功，参数化+数据驱动 获取手机号及密码
     @allure.feature("手机号登录成功")
     @pytest.mark.parametrize('param1,param2', Yamls().read_user())
-    def test_login_fail2(self, param1, param2):
-        print(param1, param2)
+    def test_login_succeed(self, param1, param2):
         r = self.login.login(param1, param2)
         assert r.get('code') == 1
+
+    def test_loginfail(self):
+        pass
