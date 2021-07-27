e1e24d4bcb8053efa0437aecaab76556e75d20be
yuanxinghua <yuanxinghua@reworldgame.com>
2021-07-27 19:04:07 +0800

    增加allure报告样式

--- cases/settings/test_signature.py
+++ cases/settings/test_signature.py
@@ -1,28 +1,30 @@
 from api.setting_api import Settings
-
+import allure
 
 class TestSignnature:
+    # 初始化设置页
     def setup_class(self):
         self.settings = Settings()
 
-    # 修改成功签名-数字
+    @allure.feature("修改成功签名-数字")
     def test_change_signature(self, user):
         r = self.settings.changeSignature(user)
         assert  r.get('code') == 1
         assert  r.get('message') == '操作成功'
 
-    # 修改成功签名-文字
+    @allure.feature("修改成功签名-文字")
     def test_change_letter(self):
         r = self.settings.changeSignatureletter()
         assert  r.get('code') == 1
         assert  r.get('message') == '操作成功'
 
+    @allure.feature("修改成功签名-特殊符号")
     def test_change_symbol(self):
         r = self.settings.changeSignatureSymbol()
         assert r.get('code') == 1
         assert r.get('message') == '操作成功'
 
-    # 修改签名失败-敏感字
+    @allure.feature("修改签名失败-敏感字")
     def test_symbol(self):
         r = self.settings.changeSignaFail()
         assert r.get('code') == 11057
--- cases/test_login.py
+++ cases/test_login.py
@@ -1,14 +1,16 @@
 import pytest
-
+import allure
 from api.login_api import Login
 from datas.read_yaml import Yamls
 
 
 class TestLogin:
+    # 初始化用户登录
     def setup_class(self):
         self.login = Login()
 
     # 手机号登录成功，参数化+数据驱动 获取手机号及密码
+    @allure.feature("手机号登录成功")
     @pytest.mark.parametrize('param1,param2', Yamls().read_user())
     def test_login_fail2(self, param1, param2):
         print(param1, param2)
