895ad2fbe47b222fe66d421ad56f22c47f61e33b
yuanxinghua <yuanxinghua@reworldgame.com>
2021-07-28 09:53:22 +0800

    优化用例

--- cases/settings/test_signature.py
+++ cases/settings/test_signature.py
@@ -8,25 +8,25 @@ class TestSignnature:
     def setup_class(self):
         self.settings = Settings()
 
-    @allure.feature("修改成功签名-数字")
+    @allure.story("修改成功签名-数字")
     def test_change_signature(self, user):
         r = self.settings.changeSignature(user)
         assert  r.get('code') == 1
         assert  r.get('message') == '操作成功'
 
-    @allure.feature("修改成功签名-文字")
+    @allure.story("修改成功签名-文字")
     def test_change_letter(self, user):
         r = self.settings.changeSignatureletter(user)
         assert  r.get('code') == 1
         assert  r.get('message') == '操作成功'
 
-    @allure.feature("修改成功签名-特殊符号")
+    @allure.story("修改成功签名-特殊符号")
     def test_change_symbol(self, user):
         r = self.settings.changeSignatureSymbol(user)
         assert r.get('code') == 1
         assert r.get('message') == '操作成功'
 
-    @allure.feature("修改签名失败-敏感字")
+    @allure.story("修改签名失败-敏感字")
     def test_symbol(self, user):
         r = self.settings.changeSignaFail(user)
         assert r.get('code') == 11057
