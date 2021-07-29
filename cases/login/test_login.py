ed6be60230da75ee111cf03951826db5595b72ab
yuanxinghua <yuanxinghua@reworldgame.com>
2021-07-29 18:45:46 +0800

    优化手机号登录用例

--- cases/login/test_login.py
+++ cases/login/test_login.py
@@ -4,17 +4,41 @@ from api.login_api import Login
 from datas.read_yaml import Yamls
 
 
+@allure.feature("登录页")
 class TestLogin:
     # 初始化用户登录
     def setup_class(self):
         self.login = Login()
 
     # 手机号登录成功，参数化+数据驱动 获取手机号及密码
-    @allure.feature("手机号登录成功")
-    @pytest.mark.parametrize('param1,param2', Yamls().read_user())
+    @allure.story("手机号登录成功")
+    @pytest.mark.parametrize('param1,param2', Yamls().read_user()['data'])
     def test_login_succeed(self, param1, param2):
         r = self.login.login(param1, param2)
         assert r.get('code') == 1
 
-    def test_loginfail(self):
-        pass
+    @allure.story("手机号登录失败-密码纯英文")
+    @pytest.mark.parametrize('param1,param2', Yamls().read_user()['data2'])
+    def test_loginfail_letter(self, param1, param2):
+        r = self.login.login(param1, param2)
+        assert r.get('code') == 11011
+
+    @allure.story("手机号登录失败-密码纯数字")
+    @pytest.mark.parametrize('param1,param2', Yamls().read_user()['data3'])
+    def test_loginfail_figure(self, param1, param2):
+        r = self.login.login(param1, param2)
+        assert r.get('code') == 11011
+
+    @allure.story("手机号登录失败-密码特殊符号")
+    @pytest.mark.parametrize('param1', Yamls().read_user()['data4'])
+    def test_loginfail_symbol(self, param1):
+        param2 = "@%^&#%^&"
+        r = self.login.login(param1, param2)
+        assert r.get('code') == 11011
+
+    @allure.story("手机号登录失败-密码为空")
+    @pytest.mark.parametrize('param1', (Yamls().read_user()['data4']))
+    def test_loginfail_empty(self, param1):
+        param2 = "        "
+        r = self.login.login(param1, param2)
+        assert r.get('code') == 25005
--- datas/read_yaml.py
+++ datas/read_yaml.py
@@ -3,6 +3,7 @@ import os
 
 
 class Yamls:
+    # 获取配置域名+端口号
     def read_yml(self):
         """
         获取当前环境的绝对路径
@@ -15,6 +16,7 @@ class Yamls:
             data = yaml.safe_load(f.read())
         return data
 
+    # 正确的账号和密码
     def read_user(self):
         base_path = os.path.dirname(os.path.realpath(__file__))
         # 获取当前配置文件的路径，相当于根路径，读取config.yaml配置文件
@@ -23,11 +25,20 @@ class Yamls:
             data = yaml.safe_load(f.read())
         return data
 
-# if __name__ == '__main__':
+    def read_user_fail(self):
+        base_path = os.path.dirname(os.path.realpath(__file__))
+        # 获取当前配置文件的路径，相当于根路径，读取config.yaml配置文件
+        config_path = os.path.join(base_path, 'user.yaml')
+        with open(config_path, 'r', encoding='utf-8') as f:
+            data = yaml.safe_load(f.read())
+        return data
+
+
+if __name__ == '__main__':
 #     # print(read_yml()['env200']['host'])
 
-# print(Yamls().read_user())
-# print(Yamls().read_user())
+print(Yamls().read_user()['data'])
+print(Yamls().read_user()['data4'][0])
 #     url = read_yml()['env200']['host'] + ':'+json.dumps(read_yml()['env200']['port'])
 #     print(url)
 #     print(type(url))
