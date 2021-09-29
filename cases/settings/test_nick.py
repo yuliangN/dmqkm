8d21fb7ae8b8d94e420616738ff6899a4b4c6770
yuanxinghua <yuanxinghua@reworldgame.com>
2021-09-29 14:04:08 +0800

    优化：调整用例顺序

--- cases/settings/test_nick.py
+++ cases/settings/test_nick.py
@@ -9,6 +9,16 @@ class TestNick:
     # 初始化设置页
     def setup_class(self):
         self.settings = Settings()
+    @allure.story("修改失败-昵称敏感字")
+    def test_nick_sensitive(self, user):
+        r = self.settings.nickSensitiveWord(user)
+        assert r.get('code') == 11057
+
+    @allure.story("修改失败-昵称为空（后端未做校验）")
+    def test_nick_null(self, user):
+        r = self.settings.nickNull(user)
+        assert r.get('code') == 1
+        assert r.get('message') == '操作成功'
 
     @allure.story("修改成功昵称")
     def test_success_nick(self, user):
@@ -21,14 +31,3 @@ class TestNick:
         r = self.settings.nickExist(user)
         assert r.get('code') == 11022
         assert r.get('message') == '昵称已存在'
-
-    @allure.story("修改失败-昵称敏感字")
-    def test_nick_sensitive(self, user):
-        r = self.settings.nickSensitiveWord(user)
-        assert r.get('code') == 11057
-
-    @allure.story("修改失败-昵称为空（后端未做校验）")
-    def test_nick_null(self, user):
-        r = self.settings.nickNull(user)
-        assert r.get('code') == 1
-        assert r.get('message') == '操作成功'
