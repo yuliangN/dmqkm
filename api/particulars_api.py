b00bbc28dbe3edfc12b2984375b2ed26a8d2f8fe
yuanxinghua <yuanxinghua@reworldgame.com>
2021-10-27 18:45:46 +0800

    新增：删除测试用例

--- api/particulars_api.py
+++ api/particulars_api.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 from datas.read_yaml import users
 from lib.api_template import ApiTemplate
+from lib.config_sql import db
 from lib.log_api import log
 
 
@@ -35,6 +36,19 @@ class Particulars:
         return res_json
 
     # 删除评论
+    def review_delete(self, token, param):
+        id = db.review_first(''.join(list(map(str, param))))
+        path = '/review/game/remove'
+        data = {"token": token,
+                'deviceId': users.read_yml()['public']['deviceId'],
+                "lang": users.read_yml()['public']['lang'],
+                "sign": users.read_yml()['public']['sign'],
+                "osType": users.read_yml()['public']['osType'],
+                "id": id
+                }
+        res_json = ApiTemplate().post_api(path, data)
+        log.info(f"接口返回结果为 ：{res_json}")
+        return res_json
 
     # 举报评论
 
